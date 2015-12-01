2200272d65cca52b09c8ae8754a5f764e5f1ac1c
Andreas Fritsch <andreas.fritsch@posteo.de>
2015-12-01 22:24:59 +0100

    rudimentary functionality

---
+++ server/methods.js
@@ -0,0 +1,31 @@
+Meteor.methods({
+  setSubscription: function(dateId, email, subState) {
+    var userId = Users.findOne({email: email})._id;
+    console.log(userId);
+    var subscribers = Dates.findOne({_id: dateId}, {fields: {subscribers: 1}}).subscribers;
+    console.log(subscribers.indexOf(userId));
+    if (subscribers.indexOf(userId) === -1 && subState) {
+      Dates.update({_id: dateId}, {$push: {subscribers: userId}});
+    } else if (! subState){
+      Dates.update({_id: dateId}, {$pull: {subscribers: userId}});
+    }
+
+  },
+  createDate: function(dateInfo) {
+    Dates.insert({
+      dateInfo: dateInfo,
+      subscribers: []
+    });
+  },
+  createUser: function(email) {
+    Users.insert({
+      email: email
+    });
+  },
+  deleteUser: function (userId) {
+    Users.remove(userId);
+  },
+  deleteDate: function (dateId) {
+    Dates.remove(dateId);
+  }
+});
--- server/publications.js
+++ server/publications.js
@@ -1,1 +1,7 @@
 // Meteor.publish definitions
+Meteor.publish("dates", function () {
+    return Dates.find({});
+});
+Meteor.publish("users", function () {
+    return Users.find({});
+});

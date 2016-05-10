4905ff6da49ba84b58fb33002605af52f49bb3fb
Samuel Lutzweiler <samuel.lutzweiler@posteo.de>
2016-05-10 21:05:31 +0200

    user account spieler erstellen

--- client/date.js
+++ client/date.js
@@ -7,103 +7,122 @@ Session.set('loggedInUserDisplay', "");
 Session.set('loggedInUserFullname', "");
 
 Template.date.onRendered(function() {
-  this.autorun(function() {
-	if(Session.get('loggedInUserFullname') != "") {
-	  // User is logged in
-	  classie.remove(document.querySelector(".user-account"), 'is-hidden');
-	  classie.add(document.querySelector(".user-account"), 'is-visible');
-	} else {
-	  // User is logged out
-	  classie.remove(document.querySelector(".user-account"), 'is-visible');
-	  classie.add(document.querySelector(".user-account"), 'is-hidden');
-	}
-  });
+    this.autorun(function() {
+        if (Session.get('loggedInUserFullname') != "") {
+            // User is logged in
+            classie.remove(document.querySelector(".user-account"), 'is-hidden');
+            classie.add(document.querySelector(".user-account"), 'is-visible');
+        } else {
+            // User is logged out
+            classie.remove(document.querySelector(".user-account"), 'is-visible');
+            classie.add(document.querySelector(".user-account"), 'is-hidden');
+        }
+    });
 });
 
 Template.date.helpers({
-	currentUser: function() {
-		return Users.findOne({
-			_id: Session.get("currentUser")
-		});
-	},
-	subscribers: function() {
-		//if(!this._id)
-		//	return;
-		//var subscriberIds = DatesUsers.find({date_id: this._id}).fetch();
-		var subscribers = DatesUsers.find({
-			date_id: this._id,
-			state: true
-		}, {
-			fields: {
-				'user_id': 1
-			}
-		}).fetch();
-		var subscriberIds = [];
-		for (var i = 0; i <= subscribers.length; i++) {
-			if (subscribers[i])
-				subscriberIds.push(subscribers[i]['user_id']);
-		}
-		console.log(subscriberIds);
-		//return [];
-		return Users.find({
-			_id: {
-				$in: subscriberIds
-			}
-		});
-	},
-	numSubscribers: function(subState) {
-		return DatesUsers.find({
-			date_id: this._id,
-			state: subState
-		}, {
-			fields: {
-			'user_id': 1
-		}
-		}).count();
-		//console.log("numSubs" + countSubscribers(subState));
-		//return countSubscribers(subState);
-	},
-	datestate: function() {
-		var subs = DatesUsers.find({
-			date_id: this._id,
-			state: true
-		}, {
-			fields: {
-			'user_id': 1
-		}
-		}).count();
-		if (subs < 6) {
-			if (new Date() > this.date - 3600000) {
-				return	"Nein, leider nicht!";
-			} else {
-				return "Hm.. noch sind wir zu wenig!";
-			}
-		} else {
-			return "Ja, voll geil man!";
-		}
-	},
-	isSubscripted: false,
-	
-	dates: function() {
-		return Dates.find({}, {
-			sort: {
-				date: 1
-			}
-		});
-	},
-	users: function() {
-		return Users.find({});
-	}
+    currentUser: function() {
+        return Users.findOne({
+            _id: Session.get("currentUser")
+        });
+    },
+    subscribers: function() {
+        //if(!this._id)
+        //	return;
+        //var subscriberIds = DatesUsers.find({date_id: this._id}).fetch();
+        var subscribers = DatesUsers.find({
+            date_id: this._id,
+            state: true
+        }, {
+            fields: {
+                'user_id': 1
+            }
+        }).fetch();
+        var subscriberIds = [];
+        for (var i = 0; i <= subscribers.length; i++) {
+            if (subscribers[i])
+                subscriberIds.push(subscribers[i]['user_id']);
+        }
+        console.log(subscriberIds);
+        //return [];
+        return Users.find({
+            _id: {
+                $in: subscriberIds
+            }
+        });
+    },
+    numSubscribers: function(subState) {
+        return DatesUsers.find({
+            date_id: this._id,
+            state: subState
+        }, {
+            fields: {
+                'user_id': 1
+            }
+        }).count();
+        //console.log("numSubs" + countSubscribers(subState));
+        //return countSubscribers(subState);
+    },
+    datestateClass: function() {
+        var subs = DatesUsers.find({
+            date_id: this._id,
+            state: true
+        }, {
+            fields: {
+                'user_id': 1
+            }
+        }).count();
+        if (subs < 6) {
+            if (new Date() > this.date - 3600000) {
+                return "no";
+            } else {
+                return "maybe";
+            }
+        } else {
+            return "yes";
+        }
+    },
+    datestate: function() {
+        var subs = DatesUsers.find({
+            date_id: this._id,
+            state: true
+        }, {
+            fields: {
+                'user_id': 1
+            }
+        }).count();
+        if (subs < 6) {
+            if (new Date() > this.date - 3600000) {
+                return "Nein, leider nicht!";
+            } else {
+                return "Hm, noch sind wir zu wenig... meld dich doch an!";
+            }
+        } else {
+            return "Ja, voll geil man!";
+        }
+    },
+    isSubscripted: false,
+
+    dates: function() {
+        return Dates.find({}, {
+            sort: {
+                date: 1
+            }
+        });
+    },
+    users: function() {
+        return Users.find({});
+    }
 });
 
 Template.date.events({
     "click .unsubscribe-button": function(event, template) {
         event.preventDefault();
-		var user = Session.get('loggedInUserFullname') || template.find('#subscriber-email').value;
+        var user = Session.get('loggedInUserFullname') || template.find('#subscriber-email').value;
         Meteor.call('setSubscription', this._id, user, false, function(error, result) {
             if (result) {
-				Session.set('loggedInUserDisplay', result.prename);
-				Session.set('loggedInUserFullname', result.fullname);
+                Session.set('loggedInUserDisplay', result.prename);
+                Session.set('loggedInUserFullname', result.fullname);
                 document.getElementById('subscriber-email').disabled = true;
                 document.getElementById('subscriber-email').value = result.prename + ' will leider nicht mitkicken!';
                 classie.add(document.querySelector(".unsubscribe-button"), 'is-hidden');
@@ -116,14 +135,14 @@ Template.date.events({
     },
     "click .subscribe-button": function(event, template) {
         event.preventDefault();
-		var user = Session.get('loggedInUserFullname') || template.find('#subscriber-email').value;
+        var user = Session.get('loggedInUserFullname') || template.find('#subscriber-email').value;
         Meteor.call('setSubscription', this._id, user, true, function(error, result) {
             if (result) {
-				Session.set('loggedInUserDisplay', result.prename); 
-				Session.set('loggedInUserFullname', result.fullname); 
+                Session.set('loggedInUserDisplay', result.prename);
+                Session.set('loggedInUserFullname', result.fullname);
 
-            	//console.log(Users.findOne({_id: Session.get("currentUser")}).email);
-                
+                //console.log(Users.findOne({_id: Session.get("currentUser")}).email);
+
                 document.getElementById('subscriber-email').disabled = true;
                 document.getElementById('subscriber-email').value = result.prename + ' ist volle Kanne dabei!';
                 classie.add(document.querySelector(".unsubscribe-button"), 'undo');
@@ -138,19 +157,19 @@ Template.date.events({
     "click .user-account__option--new-player": function(event, template) {
         classie.add(document.querySelector(".user-account__menu--new-player"), 'is-visible');
     },
-	"click .btn__add-user": function(event, template) {
-	  Session.set('loggedInUserFullname', "");
-	  Session.set('loggedInUserDisplay', "");
+    "click .btn__add-user": function(event, template) {
+        Session.set('loggedInUserFullname', "");
+        Session.set('loggedInUserDisplay', "");
 
-	  document.getElementById('subscriber-email').disabled = false;
-	  document.getElementById('subscriber-email').value = "";
-      classie.remove(document.querySelector(".unsubscribe-button"), 'undo');
-      classie.remove(document.querySelector(".unsubscribe-button"), 'is-hidden');
-      classie.remove(document.querySelector(".subscribe-button"), 'undo');
-      classie.remove(document.querySelector(".subscribe-button"), 'is-hidden');
-	  document.getElementsByClassName('unsubscribe-button-text')[0].innerHTML = 'Ohne mich!';
-	  document.getElementsByClassName('subscribe-button-text')[0].innerHTML = 'Ich kicke mit!';
+        document.getElementById('subscriber-email').disabled = false;
+        document.getElementById('subscriber-email').value = "";
+        classie.remove(document.querySelector(".unsubscribe-button"), 'undo');
+        classie.remove(document.querySelector(".unsubscribe-button"), 'is-hidden');
+        classie.remove(document.querySelector(".subscribe-button"), 'undo');
+        classie.remove(document.querySelector(".subscribe-button"), 'is-hidden');
+        document.getElementsByClassName('unsubscribe-button-text')[0].innerHTML = 'Ohne mich!';
+        document.getElementsByClassName('subscribe-button-text')[0].innerHTML = 'Ich kicke mit!';
 
-	  document.getElementById("subscriber-email").focus();
-	}
+        document.getElementById("subscriber-email").focus();
+    }
 });
--- client/mainLayout.js
+++ client/mainLayout.js
@@ -3,3 +3,24 @@ Template.mainLayout.helpers({
 	return Session.get('loggedInUserDisplay');
   }
 });
+Template.mainLayout.events({
+	 "submit .new-player-form": function (event) {
+		 // Prevent default browser form submit
+        event.preventDefault();
+    	console.log('hiih');
+
+        // Get value from form element
+        var prename_text = event.target.prename.value;
+        var surname_text = event.target.surname.value;
+        var email_text = event.target.email.value;
+        var balance_input = 0;
+
+        // Insert a user into the collection
+        Meteor.call("createUser", prename_text, surname_text, email_text, balance_input);
+
+        // Clear form
+        event.target.email.value = "";
+        event.target.prename.value = "";
+        event.target.surname.value = "";
+    }
+});
